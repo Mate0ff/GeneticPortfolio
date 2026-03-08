9caea3467a37bc8bf36ca94bb40e6ce62628e88d
Mate0ff <mkucharz4@gmail.com>
2026-03-08 21:25:49 +0100

    add fitnes func, started cross_breed

--- ga_module.py
+++ ga_module.py
@@ -4,13 +4,14 @@ import numpy as np
 import random
 
 
-
-
 class GeneticPortfolio():
 
     def __init__(self, data):
         self._tickers = self.__extract_tickers(data) 
         self._data = self.__calculate_returns(data)
+        self._cov_matrix = self.__calculate_cov_matrix(self._data)
+        self.__symbol_means = self.__calculate_mean_returns(self._data)
+        self._population_size = None
 
     @staticmethod
     def __extract_tickers(data):
@@ -18,16 +19,26 @@ class GeneticPortfolio():
     
     @staticmethod
     def __calculate_returns(data):
-        data['returns'] = data.groupby('symbol')['close'].pct_change().fillna(0)
-        return data
+        df = data.copy()
+        df['returns'] = df.groupby('symbol')['close'].pct_change().fillna(0)
+        return df
+    
+    @staticmethod
+    def __calculate_cov_matrix(data):
+        returns_matrix = data.pivot(index='timestamp', columns='symbol', values='returns')
+        return returns_matrix.cov()
 
+    @staticmethod
+    def __calculate_mean_returns(data):
+        return data.groupby('symbol')['returns'].mean()
+    
     def get_data(self):
         return self._data
     
     def get_tickers(self):
         return self._tickers
 
-    def _get_portfolio(self, portfolio_size) -> dict: 
+    def __get_portfolio(self, portfolio_size) -> dict: 
 
         current_tickers = random.sample(self._tickers, portfolio_size)
         weights = np.random.rand(portfolio_size)
@@ -36,29 +47,94 @@ class GeneticPortfolio():
         return dict(zip(current_tickers,normalized_weights))
 
     def get_population(self, pop_size = 400) -> list:
-
+        self._population_size = pop_size
         population = [
-                    self._get_portfolio(random.randint(3,20)) 
-                    for _ in range(pop_size)
+                    self.__get_portfolio(random.randint(3,20)) 
+                    for _ in range(self._population_size)
                     ]
 
         return population
-
-    def get_returns(self):
-
-        for i in self._tickers:
-            ticker_data = self._data[self._data['symbol'] == i]
-            ticker_data = ticker_data['close']
-            ticker_data.pct_change()
             
-    def evaluate_portfolios(generation):
-        # sharpe ratio for now 
+    def evaluate_portfolios(self,generation):
+        fitnes = []
+        
+        for p in generation:
+            
+            symbols = list(p.keys())
+            weights = np.array(list(p.values()))
+
+            portfolio_return = np.sum(self.__symbol_means[symbols] * weights) 
+  
+            portfolio_cov = self._cov_matrix.loc[symbols, symbols].values
+            portfolio_variance = np.dot(weights.T, np.dot(portfolio_cov, weights))
+            volatility = np.sqrt(portfolio_variance) if portfolio_variance > 0 else 1e-9
+
+            fitnes.append(portfolio_return/volatility)
+
+        return np.array(fitnes)
+
+    def get_next_gen(self,generation,fitnes):
+        
+
+        # top 10 go to next gen
+        best_portfolios_idx = np.argpartition(fitnes,-10)[-10:]
+        portfolios = [generation[i] for i in best_portfolios_idx] 
+
+        # torunament
+        for _ in range(self._population_size):
+            p1, p2 = self.__get_parents(generation, fitnes)
+            c1, c2 = self.__cros_breed(p1, p2)
+
+        return 
+
+    def __cros_breed(self, p1, p2):
+        choices = list(set(p1.keys()) | set(p2.keys()))
+        avg_size = (len(p1) + len(p2)) / 2
+        child_size = int(np.clip(round(avg_size) + + random.randint(-1, 1), 3, 20))
+        child_size = min(child_size, len(choices))
+
+        def create_one_child():
+            tickers = np.random.choice(choices, size=child_size, replace=False)
+            w_values = np.zeros(len(tickers))
+            
+            for i, t in enumerate(tickers):
+                if t in p1 and t in p2:
+                    w_values[i] = (p1[t] + p2[t]) / 2.0
+                elif t in p1:
+                    w_values[i] = p1[t]
+                else:
+                    w_values[i] = p2[t]
+
+            sum_w = np.sum(w_values)
+            if sum_w > 0:
+                w_values /= sum_w
+            else:
+                w_values = np.ones(len(tickers)) / len(tickers)
+                
+            return dict(zip(tickers, w_values))
+
+        c1 = create_one_child()
+        c2 = create_one_child()
+
+        return c1, c2
+    
+    def __mutate(self, c1, c2,chance = 0.02):
         pass
 
-    def get_portfolio_score(portfolio):
-        pass
+    def __get_parents(self, generation, fitnes, n_turnament=10):
+        parents = []
+        
+        while len(parents) < 2:
+            possible_portfolios = np.random.choice(self._population_size, n_turnament, replace=False)
+            
+            fitnes_scores = fitnes[possible_portfolios]
+            best_one_idx = possible_portfolios[np.argmax(fitnes_scores)]
 
-    def get_parents():
-        pass
+            if best_one_idx not in parents:
+                parents.append(best_one_idx)
+                
+        return generation[parents[0]], generation[parents[1]]
+
+
     
 
